bccafd85c7d32b6f5ce01119cf9f5c83d4803d55
keyinstudent <mike.wadden@keyin.com>
2022-04-12 23:14:42 -0230

    authform work

--- garyblue/src/componet/Auth/AuthForm.jsx
+++ garyblue/src/componet/Auth/AuthForm.jsx
@@ -1,25 +1,55 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 import classes from "./AuthForm.module.css";
 
 const AuthForm = () => {
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+  const nameInputRef = useRef();
   const [isLogin, setIsLogin] = useState(true);
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
   };
 
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const email = emailInputRef.current.target;
+    const password = passwordInputRef.current.target;
+    const name = nameInputRef.current.target;
+  };
+
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? "Login" : "Sign Up"}</h1>
-      <form>
+      <form onSubmit={submitHandler}>
+        {!isLogin && (
+          <div className={classes.control}>
+            <label htmlFor="name">Enter Your Name</label>
+            <input
+              maxlength="25"
+              type="text"
+              id="name"
+              pattern="[a-zA-Z]+"
+              required
+              ref={nameInputRef}
+            />
+          </div>
+        )}
         <div className={classes.control}>
-          <label htmlFor="email">Your Email</label>
-          <input type="email" id="email" required />
+          <label htmlFor="email"> Your Email</label>
+          <input type="email" id="email" required ref={emailInputRef} />
         </div>
         <div className={classes.control}>
-          <label htmlFor="password">Your Password</label>
-          <input type="password" id="password" required />
+          <label htmlFor="password"> Your Password</label>
+          <input
+            type="password"
+            id="password"
+            required
+            ref={passwordInputRef}
+            minlength="5"
+            maxlength="10"
+          />
         </div>
         <div className={classes.actions}>
           <button>{isLogin ? "Login" : "Create Account"}</button>
